be76f5fe54bfcaacbce1b32f6ac8eef2fb474b1a
kimhaneui <kimhaneui2@naver.com>
2020-05-27 15:13:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/api_0527/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.api_0527;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.api_0527.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
